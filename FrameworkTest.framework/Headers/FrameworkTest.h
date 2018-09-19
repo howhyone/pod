78a512634b8295eb923bc291e7b927a3ae1db428
mob <zx14993@163.com>
2018-09-19 12:34:50 +0800

    pod First commit

---
+++ FrameworkTest.framework/Headers/FrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTest.h
+//  FrameworkTest
+//
+//  Created by mob on 2018/8/8.
+//  Copyright © 2018年 mob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkTest.
+FOUNDATION_EXPORT double FrameworkTestVersionNumber;
+
+//! Project version string for FrameworkTest.
+FOUNDATION_EXPORT const unsigned char FrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTest/PublicHeader.h>
+
+#import "ShowResult.h"
---
+++ FrameworkTest.framework/Headers/ShowResult.h
@@ -0,0 +1,15 @@
+//
+//  ShowResult.h
+//  FrameworkTest
+//
+//  Created by mob on 2018/8/6.
+//  Copyright © 2018年 mob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShowResult : NSObject
+
++(void)show;
+
+@end
